629e9ee5501f552eba2f9552d468dfda2589c9f9
radungureanu <u.radu04@gmail.com>
2025-09-25 23:10:21 +0300

    Delete VOIS_Team6 directory

--- VOIS_Team6/app/init_db.py
+++
@@ -1,7 +0,0 @@
-from database import Base, engine
-from models import Task, User
-
-# creează toate tabelele definite în models
-Base.metadata.create_all(bind=engine)
-
-print("Tabelele au fost create cu succes!")
